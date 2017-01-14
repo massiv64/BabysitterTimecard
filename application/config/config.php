6aa8147fed3422868e65b7b405130566fe1a5584
Travis Neal <nl.travis@yahoo.com>
2017-01-14 00:46:45 -0800

    base url is now elastic to the current host.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://timecard.dev';
+$config['base_url'] = "http".((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "s" : "")."://".$_SERVER['HTTP_HOST']."/";
 
 /*
 |--------------------------------------------------------------------------
