b36a52a47b6bbb8c012161f4aca930717a42e927
Travis Neal <nl.travis@yahoo.com>
2017-01-24 02:03:05 -0800

    quick test page.

--- public/application/views/welcome_message.php
+++ public/application/views/welcome_message.php
@@ -1,89 +1,82 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+<html>
+    <body>
+        <button type="button" id="clock-in">Clock In</button>
+        <div id="clock-in-result"></div>
+        <br/>
+        <button type="button" id="clock-out">Clock Out</button>
+        <div id="clock-out-result"></div>
+        <br/>
+        <button type="button" id="get-all-times">Get All Times</button>
+        <div id="get-all-times-result"></div>
+        <br/>
 
-	<style type="text/css">
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"
+                integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+                crossorigin="anonymous"></script>
+        <script>
+            (function($){
+                $('#clock-in').on('click', function(e){
+                    e.preventDefault();
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
+                    $.ajax({
+                        type: "POST",
+                        data: {
+                            time: new Date()
+                        },
+                        url: "/welcome/insert_clock_in",
+                        success: function (result) {
+                            console.log(result);
+                            $("#clock-in-result").html(result);
+                        },
+                        error: function (result) {
+                            console.log(result);
+                            $("#clock-in-result").html(result.responseText);
+                        }
+                    });
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
+                    return false;
+                });
+                $('#clock-out').on('click', function(e){
+                    e.preventDefault();
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
+                    $.ajax({
+                        type: "POST",
+                        data: {
+                            time: new Date()
+                        },
+                        url: "/welcome/insert_clock_out",
+                        success: function (result) {
+                            console.log(result);
+                            $("#clock-out-result").html(result);
+                        },
+                        error: function (result) {
+                            console.log(result);
+                            $("#clock-out-result").html(result.responseText);
+                        }
+                    });
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
+                    return false;
+                });
+                $('#get-all-times').on('click', function(e){
+                    e.preventDefault();
 
-	#body {
-		margin: 0 15px 0 15px;
-	}
+                    $.ajax({
+                        type: "GET",
+                        url: "/welcome/get_all_times",
+                        success: function (result) {
+                            console.log(result);
+                            $("#get-all-times-result").html(result);
+                        },
+                        error: function (result) {
+                            console.log(result);
+                            $("#get-all-times-result").html(result.responseText);
+                        }
+                    });
 
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
-</body>
+                    return false;
+                });
+            })(jQuery);
+        </script>
+    </body>
 </html>
